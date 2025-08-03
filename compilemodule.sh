10788657bb0357a8b06822525a10271fa2d9cb75
FJOX.win <info@fjox.win>
2025-08-03 17:49:56 +0200

    removed the dependancy shit stuff

--- compilemodule.sh
+++ compilemodule.sh
@@ -76,19 +76,10 @@ fi
 
 bash -c "$commands"
 
-# Get Dependancies
-echo "Downloading Dependancies"
-cd "$nginx_location/"
-cd ../
-git clone git://git.openssl.org/openssl.git
-wget https://www.zlib.net/zlib-1.3.1.tar.gz
-tar -xzvf zlib-1.3.1.tar.gz
-mv zlib-1.3.1 zlib
-
 # Do configure
 cd "$nginx_location/"
 
-cmd="./configure --with-compat --with-openssl=../openssl --with-zlib=../zlib $args"
+cmd="./configure --with-compat $args"
 $cmd
 
 # Do make
